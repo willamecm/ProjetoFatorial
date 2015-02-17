0678b12b77af507b653b909d8ecea10375823ae8
Francisco Willame Catariono de Mesquita <willamecm@gmail.com>
2015-02-17 19:35:09 -0200

    comentarios em fatorial.java atualizados

--- Fatorial.java
+++ Fatorial.java
@@ -1,6 +1,6 @@
 public class Fatorial {
 	
-	//versão iterativa do calculo do fatorial
+	//versão iterativa do calculo do fatorial, incluindo o teste para n=0
 	public int calcularFatorial(int n) throws IllegalArgumentException{
 		
 		int resultado = 1;
