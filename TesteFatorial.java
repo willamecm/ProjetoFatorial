e1c8719c01288294327e5a0b7ff0561fdd0d0f4f
Francisco Willame Catariono de Mesquita <willamecm@gmail.com>
2015-02-17 19:49:59 -0200

    calculo do fatorial de 4 em vez de 0

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -4,7 +4,7 @@ public class TesteFatorial{
 	public static void main(String [] args){
 
 		Fatorial fat = new Fatorial();
-		int resultado = fat.calcularFatorial(0);
+		int resultado = fat.calcularFatorial(4);
 		System.out.println(resultado);
 	}
 }
