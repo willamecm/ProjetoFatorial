f91791a06f62998bee0edeac7e9e23a86287ea6d
Francisco Willame Catariono de Mesquita <willamecm@gmail.com>
2015-02-17 17:21:47 -0200

    alteracao para o calculo correto do fatorial de 0

--- Fatorial.java
+++ Fatorial.java
@@ -2,12 +2,17 @@ public class Fatorial {
 	
 	//versão iterativa do calculo do fatorial
 	public int calcularFatorial(int n) throws IllegalArgumentException{
+		
+		int resultado = 1;
+
 		if(n < 0)
 			throw new IllegalArgumentException("não existe fatorial para numeros negativos");
-		int resultado = 1;
 		
-		for(int i = 2; i <= n; i++)
-			resultado *= i;
+		if(n != 0){
+			for(int i = 2; i <= n; i++)
+				resultado *= i;
+		}
+
 		return resultado;
 	}
 }
--- TesteFatorial.java
+++ TesteFatorial.java
@@ -4,7 +4,7 @@ public class TesteFatorial{
 	public static void main(String [] args){
 
 		Fatorial fat = new Fatorial();
-		int resultado = fat.calcularFatorial(-5);
+		int resultado = fat.calcularFatorial(0);
 		System.out.println(resultado);
 	}
 }
