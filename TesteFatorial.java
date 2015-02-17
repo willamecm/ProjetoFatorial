a3acef40628c172b2723aa66a5752c979066bdbf
Francisco Willame Catariono de Mesquita <willamecm@gmail.com>
2015-02-17 12:17:34 -0200

    mudanças na classe testefatorial e fatorial

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,9 +1,10 @@
 public class TesteFatorial{
-
+	
+	//comentario
 	public static void main(String [] args){
 
 		Fatorial fat = new Fatorial();
-		int resultado = fat.calcularFatorial(5)
+		int resultado = fat.calcularFatorial(5);
 		System.out.println(resultado);
 	}
 }
